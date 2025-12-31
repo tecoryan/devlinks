8f1ce4aa7097cd374a9669309add8295dd8e457c
tecoryan <63824518+tecoryan@users.noreply.github.com>
2025-12-31 10:04:54 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function toggleMode() {
+    const html = document.documentElement
+    html.classList.toggle('light')
+
+    // pegar a tag img
+    const imagem = document.querySelector('#profile img')
+    //substituir a imagem
+    if(html.classList.contains('light')) {
+            // se tiver light mnode, adicionar a imagem light
+        imagem.setAttribute('src', 'avatar-ps.png')
+    } else {
+        // se sem light mode, substituir a imagem
+        imagem.setAttribute('src', 'avatar.png')
+    }
+}
